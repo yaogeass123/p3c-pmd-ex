512bf9ea1d68ac102265351225ad32412f44aca9
polaris <yaochenguang@dianwoda.com>
2019-11-19 10:32:47 +0800

    StringBuilder

--- p3c-pmd/src/main/java/com/alibaba/p3c/pmd/lang/java/rule/ex/LogErrorRule.java
+++ p3c-pmd/src/main/java/com/alibaba/p3c/pmd/lang/java/rule/ex/LogErrorRule.java
@@ -24,11 +24,11 @@ public class LogErrorRule extends AbstractAliRuleEx {
 
     private static final String MESSAGE_KEY_PREFIX = "java.ex.LogErrorRule.rule.msg";
     private static final String LOGGER_PACKAGE = "org.slf4j.Logger";
-    private static final String partter = "@Image='$$$$$.info'";
-    private static final String targetChildXPath =
+    private static final String PATTER = "@Image='%s.info'";
+    private static final String TARGET_XPATH_FORM =
             "//Statement/StatementExpression/PrimaryExpression"
-                    + "[PrimaryPrefix/Name[&&&&&] and PrimarySuffix/Arguments/ArgumentList/"
-                    + "Expression/PrimaryExpression/PrimaryPrefix/Name[@Image='#####']]";
+                    + "[PrimaryPrefix/Name[%s] and PrimarySuffix/Arguments/ArgumentList/"
+                    + "Expression/PrimaryExpression/PrimaryPrefix/Name[@Image='%s']]";
 
     private static Map<String, String> importPackage = Maps.newHashMap();
     private static Set<String> loggerName = Sets.newHashSet();
@@ -61,11 +61,18 @@ public class LogErrorRule extends AbstractAliRuleEx {
             return super.visit(node, data);
         }
         String varName = node.getExceptionName();
-        String replace = targetChildXPath.replace("#####", varName);
-        for (String name : loggerName) {
-            replace = replace.replace("&&&&&", partter.replace("$$$$$", name) + " or &&&&&");
+        StringBuilder value = new StringBuilder();
+        boolean firstTime = true;
+        for (String logName : loggerName) {
+            if (firstTime) {
+                value.append(String.format(PATTER, logName));
+                firstTime = false;
+                continue;
+            }
+            value.append(" or ");
+            value.append(String.format(PATTER, logName));
         }
-        replace = replace.replace(" or &&&&&", "");
+        String replace = String.format(TARGET_XPATH_FORM, value.toString(), varName);
         try {
             List<Node> targetNodes = node.findChildNodesWithXPath(replace);
             for (Node targetNode : targetNodes) {

f52da12d88604b63570a47e874ff1e0865bb769e
polaris <yaochenguang@dianwoda.com>
2019-12-10 19:16:24 +0800

    错误修复

--- p3c-pmd/src/main/java/com/alibaba/p3c/pmd/lang/java/rule/utils/PrimaryNodeSearch.java
+++ p3c-pmd/src/main/java/com/alibaba/p3c/pmd/lang/java/rule/utils/PrimaryNodeSearch.java
@@ -17,7 +17,7 @@ public class PrimaryNodeSearch {
         if (!(node instanceof ASTPrimaryExpression)) {
             return null;
         }
-        while (node.jjtGetChild(0) != null) {
+        while (node.jjtGetNumChildren() > 0) {
             node = node.jjtGetChild(0);
             if (node instanceof ASTPrimaryPrefix) {
                 return (ASTPrimaryPrefix) node;
@@ -32,7 +32,7 @@ public class PrimaryNodeSearch {
         }
         List<ASTPrimarySuffix> lists = node.findChildrenOfType(ASTPrimarySuffix.class);
         for (int i = lists.size() - 1; i >= 0; i--) {
-            if(lists.get(i).jjtGetChild(0) instanceof ASTArguments) {
+            if (lists.get(i).jjtGetChild(0) instanceof ASTArguments) {
                 return (ASTArguments) lists.get(i).jjtGetChild(0);
             }
         }
@@ -42,12 +42,14 @@ public class PrimaryNodeSearch {
     public static String findPrimaryName(Node node) {
         if (node instanceof ASTPrimaryExpression) {
             ASTPrimaryPrefix prefix = findPrimaryPrefix(node);
-            if (prefix != null && prefix.jjtGetChild(0) instanceof ASTName) {
+            //this与super时prefix没有子节点
+            if (prefix != null && prefix.jjtGetNumChildren() > 0 && prefix
+                    .jjtGetChild(0) instanceof ASTName) {
                 return prefix.jjtGetChild(0).getImage();
             }
             return null;
         }
-        if (node instanceof ASTPrimaryPrefix) {
+        if (node instanceof ASTPrimaryPrefix && node.jjtGetNumChildren() > 0) {
             if (node.jjtGetChild(0) instanceof ASTName) {
                 return node.jjtGetChild(0).getImage();
             }

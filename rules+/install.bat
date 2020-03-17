b6b8f2ac8769da31c14f7d63d839edf117dca6f0
yaogeass123 <31615791+yaogeass123@users.noreply.github.com>
2020-03-17 11:09:45 +0800

    Add files via upload

---
+++ rules+/install.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal enabledelayedexpansion
+for /f "delims==" %%a in ('dir *.jar /b') do (
+set "b=%%a"
+set "c=!b:~0,11!"
+if "!c!"=="p3c-common-" (
+   start winrar a -o+ -ep -aprulesets/java %%a  ex/ali-pmd.xml
+   echo %%a
+  )
+)
